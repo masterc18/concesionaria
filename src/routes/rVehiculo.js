ee12811a7bee0a15d299a95d63ae4cdd655268eb
masterc18 <carlosfrancisco12345talavera@gmail.com>
2024-11-12 18:38:03 -0600

    Se agrego la exportacion del modulo

--- src/routes/rVehiculo.js
+++ src/routes/rVehiculo.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const rVehiculo = express.Router();
 
-rVehiculo.get('/vehiculos',(req,res)=>{
-    res.render('vehiculos')
+rVehiculo.get("/vehiculos", (req, res) => {
+  res.render("vehiculos");
 });
+
+module.exports = rVehiculo;
